766fbe95478491179811531a498dc66a8b1d1b55
Okan <32766583+OkanPhlvn@users.noreply.github.com>
2019-05-11 01:34:02 +0300

    Create SayiYuvarla.c

---
+++ SayiYuvarla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+float num1 = 9.432;
+
+printf("Yuvarlanmis hali : %.2f\n", floor(num1)); // bir önceki sayıya tamamlar.
+
+printf("Yuvarlanmis hali : %.2f", ceil(num1)); // bir sonraki sayıya tamamlar.
+
+  return 0;
+}
